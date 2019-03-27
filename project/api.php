acf64c07036b558ac119e803d9e01f5d2f692ff7
sotiris <sbaibos@gmail.com>
2019-03-27 20:33:48 +0200

    making redirects according to request type

---
+++ project/api.php
@@ -0,0 +1,52 @@
+<?php 
+
+
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+
+
+$request_method = strtolower($_SERVER['REQUEST_METHOD']);
+$project_id = isset($_GET['id']);
+
+
+if($request_method === 'get' &! $project_id){
+
+    include_once 'read.php';
+
+} else if ($request_method === 'get' && $project_id){
+include_once 'read_one.php';
+
+} else if ($request_method === 'post'){
+
+    include_once 'create.php';
+} else if ($request_method === 'put') {
+
+    include_once 'update.php';
+                                    } 
+else if ($request_method === 'delete'){
+
+    include_once 'delete.php';
+} 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- project/delete.php
+++ project/delete.php
@@ -2,7 +2,7 @@
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
@@ -19,7 +19,7 @@ $project = new project($db);
  
 // get project id
 $data = json_decode(file_get_contents("php://input"));
- 
+ var_dump($data);
 // set product id to be deleted
 $project->id = $data->id;
  
