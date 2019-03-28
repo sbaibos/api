ad9d86ef637956d4c161f35f2c97d537f2a3c0f4
sotiris <sbaibos@gmail.com>
2019-03-29 01:28:05 +0200

    fixed employer to employer, add header allow options for cors

--- code.php
+++ code.php
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS `projects` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
 
 
-INSERT INTO `myprojects7`.`projects` (`id`, `name`, `emloyer`, `dateStartEnd`, `description`, `analyticalDescription`, `siteUrl`, `photo`, `technologiesUsed`, `modified`) VALUES (NULL, 'philomama', 'freelancing', '2010 -2011', 'A blog-forum -news site done in Drupal 6. The articles are categorised. According to category are fit into specific panel. New article goes to top section of panel. Older articles go to read more section automatically.The site is also multilingual and includes the rather complicated Drupal tool panels ', 'http://sbaibos.com/patrio.html', 'http://www.sbaibos.com/patrio/', 'http://sbaibos.com/img/patrio_main.jpg', 'Drupal 6, Drupal tools, CSS', CURRENT_TIMESTAMP);
+INSERT INTO `myprojects7`.`projects` (`id`, `name`, `employer`, `dateStartEnd`, `description`, `analyticalDescription`, `siteUrl`, `photo`, `technologiesUsed`, `modified`) VALUES (NULL, 'philomama', 'freelancing', '2010 -2011', 'A blog-forum -news site done in Drupal 6. The articles are categorised. According to category are fit into specific panel. New article goes to top section of panel. Older articles go to read more section automatically.The site is also multilingual and includes the rather complicated Drupal tool panels ', 'http://sbaibos.com/patrio.html', 'http://www.sbaibos.com/patrio/', 'http://sbaibos.com/img/patrio_main.jpg', 'Drupal 6, Drupal tools, CSS', CURRENT_TIMESTAMP);
 
 //create
 {
